6027e8349d60d8e4edb7d70c093c14b5a756bd27
30006830 <48032096+30006830@users.noreply.github.com>
2019-03-06 09:40:34 +1300

    Update Program.cs

--- Week2_2ndexercise/Week2_2ndexercise/Program.cs
+++ Week2_2ndexercise/Week2_2ndexercise/Program.cs
@@ -10,14 +10,18 @@ namespace Week2_2ndexercise
     {
         static void Main(string[] args)
         {
+            //Asks the user to enter a number. The number chosen by the user will be saved as a variable.
             Console.WriteLine("Please enter a number: ");
             var num1 = int.Parse(Console.ReadLine());
 
+            //a second number is requested by the program from the user. This will also be stored in a variable.
             Console.WriteLine("Enter another number: ");
             var num2 = int.Parse(Console.ReadLine());
 
+            //This creates a new variable named total then registers the 2 variables as the 2 if they are added together.
             int total = num1 + num2;
-
+            
+            //adds the 2 variables and stores the result in the total variable then displays it to the user.
             Console.WriteLine("{0} + {1} = {2}", num1, num2, total);
 
             Console.ReadLine();

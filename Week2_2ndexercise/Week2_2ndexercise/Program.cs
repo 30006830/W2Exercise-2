0f38563ef32667690c9eb2d4ae10576f5a8a47aa
Blake Jensen <30006830@student.toiohomai.ac.nz>
2019-03-06 09:29:35 +1300

    2nd exercise

---
+++ Week2_2ndexercise/Week2_2ndexercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_2ndexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            var num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            var num2 = int.Parse(Console.ReadLine());
+
+            int total = num1 + num2;
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, total);
+
+            Console.ReadLine();
+        }
+    }
+}
